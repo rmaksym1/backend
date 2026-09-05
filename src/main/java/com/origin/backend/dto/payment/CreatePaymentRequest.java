0eb93e519218044881bb11d1b16b7c9a9f6280a6
Maks <maksimronkanen940@gmail.com>
2026-09-05 13:28:51 +0300

    CI fix

--- src/main/java/com/origin/backend/dto/payment/CreatePaymentRequest.java
+++ src/main/java/com/origin/backend/dto/payment/CreatePaymentRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-
 import java.time.YearMonth;
 
 public record CreatePaymentRequest(
