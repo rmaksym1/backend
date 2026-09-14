a5acebe39fd1bd0039fc195d89d58750d13099b5
Maks <maksimronkanen940@gmail.com>
2026-09-14 22:47:46 +0300

    Create Repository, Service layers for Message

---
+++ src/main/java/com/origin/backend/repository/MessageRepository.java
@@ -0,0 +1,12 @@
+package com.origin.backend.repository;
+
+import com.origin.backend.model.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    Page<Message> findAll(Pageable pageable);
+}
