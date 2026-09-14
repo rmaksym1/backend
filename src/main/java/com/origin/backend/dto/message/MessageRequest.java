dc8be27a7bfafab019822ec4bb79a1058294477d
Maks <maksimronkanen940@gmail.com>
2026-09-14 22:47:11 +0300

    Implement Message class and DTOs

--- src/main/java/com/origin/backend/dto/message/MessageRequest.java
+++ src/main/java/com/origin/backend/dto/message/MessageRequest.java
@@ -1,4 +1,13 @@
 package com.origin.backend.dto.message;
 
-public record MessageRequest() { // TODO - SWELLCHAT MESSAGE REQUEST DTO
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record MessageRequest(
+        @NotBlank(message = "Sender cannot be blank!")
+        @Size(max = 255, message = "Sender cannot exceed 255 characters!")
+        String sender,
+        @NotBlank(message = "Content cannot be blank!")
+        @Size(max = 4096, message = "Content cannot exceed 4096 characters!")
+        String content
+) {}
